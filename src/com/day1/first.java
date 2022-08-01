22e2b4878f8b6e4f62e7867951c122f256920ca0
bmurari <murari0418@gmail.com>
2022-08-01 12:44:27 +0530

    Day1 commits

---
+++ src/com/day1/first.java
@@ -0,0 +1,17 @@
+/* the structure of the java code
+[package]
+[import <packageName>]
+[class_modifier] <class> <className>{
+public static void main(String[] args){
+Statements;
+}
+}
+ */
+
+package com.day1;
+
+public class first {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/com/day1/second.java
@@ -0,0 +1,14 @@
+package com.day1;
+
+import java.util.*;
+//util package includes all the basic input and output modules => Scanner is a part of the util package
+
+public class second{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println(sc.nextLine());
+    }
+}
+//nextLine() -> to take the input from user as a String we use it
+//nextInt() -> interger input
+// TIP : In case if we are to use any number input better use the nextLine() so that the next input are taken accordingly
