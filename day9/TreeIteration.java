c0b71dfa1dd66b69c6e7cea88e611a57c421107d
Murari Nagendra <70743564+devil056@users.noreply.github.com>
2022-08-05 15:46:21 +0530

    Iteration approach

---
+++ day9/TreeIteration.java
@@ -0,0 +1,83 @@
+import java.util.Stack;
+import java.util.function.BinaryOperator;
+
+
+class Node2 {
+    int value;
+    Node2 left;
+
+    Node2 right;
+
+    public Node2(int data) {
+        this.value = data;
+
+    }
+
+
+    public Node2 root;
+
+    public static void insert(Node2 node, int value) {
+
+        if (value < node.value) {
+            if (node.left != null) {
+                insert(node.left, value);
+            } else {
+                System.out.println("Inserted " + value + " to left " + node.value);
+                node.left = new Node2(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                insert(node.right, value);
+            } else {
+                System.out.println("Inserted " + value + " to right " + node.value);
+                node.right = new Node2(value);
+            }
+        }
+    }
+
+    public static void preOrder(Node2 root) {
+        Stack<Node2> st = new Stack();
+        st.push(root);
+        System.out.println("Pre order value : ");
+        while (!st.isEmpty()) {
+            Node2 temp = st.pop();
+            System.out.print(temp.value + "\t");
+            if (temp.right != null)
+                st.push(temp.right);
+            if (temp.left != null)
+                st.push(temp.left);
+        }
+        System.out.println();
+    }
+
+    public static void inOrder(Node2 root) {
+        Stack<Node2> st = new Stack();
+        Node2 temp = root;
+        System.out.println("In order value : ");
+        while (!st.isEmpty() || temp != null) {
+            while (temp != null) {
+                st.push(temp);
+                temp = temp.left;
+            }
+            temp = st.pop();
+            System.out.print(temp.value + "\t");
+            temp = temp.right;
+        }
+        System.out.println();
+    }
+
+}
+
+class App {
+    public static void main(String args[]) {
+        Node2 rootnode = new Node2(25);
+        rootnode.insert(rootnode, 11);
+        rootnode.insert(rootnode, 10);
+        rootnode.insert(rootnode, 30);
+        rootnode.insert(rootnode, 28);
+        rootnode.insert(rootnode, 20);
+        rootnode.insert(rootnode, 35);
+        rootnode.preOrder(rootnode);
+        rootnode.inOrder(rootnode);
+    }
+}
