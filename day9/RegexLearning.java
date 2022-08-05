e4194e71dffcaceab4a0b8b3abbf828db0f8140e
Murari Nagendra <70743564+devil056@users.noreply.github.com>
2022-08-05 14:46:26 +0530

    Completed the given node sum

--- day9/RegexLearning.java
+++ day9/RegexLearning.java
@@ -5,16 +5,16 @@ import java.util.regex.Pattern;
 
 public class RegexLearning {
 
-    public static void main(String[] args){
-        List<String> emails=new ArrayList<String>();
+    public static void main(String[] args) {
+        List<String> emails = new ArrayList<String>();
 
         emails.add("murari0418@gmail.com");
         emails.add("@gmail.com");
-        String regex="^(.+)@(.+)$";
-        Pattern pattern1=Pattern.compile(regex);
-        for(String email:emails){
-            Matcher matcher=pattern1.matcher(email);
-            System.out.println(email+" : "+matcher.matches());
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern1 = Pattern.compile(regex);
+        for (String email : emails) {
+            Matcher matcher = pattern1.matcher(email);
+            System.out.println(email + " : " + matcher.matches());
         }
     }
 }
--- day9/nodeSum.java
+++ day9/nodeSum.java
@@ -1,47 +1,83 @@
 class NodeSum {
     Node head;
     static int size;
-    class Node{
+
+    class Node {
         int num;
         Node next;
-        Node(int data){
-            this.num=data;
-            this.next=null;
+
+        Node(int data) {
+            this.num = data;
+            this.next = null;
             ++size;
         }
     }
 
-    void addNode(int data){
+    void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    void addLast(int data){
         Node newNode=new Node(data);
         if(head==null){
             head=newNode;
             return;
         }
-        newNode.next=head;
-        head=newNode;
+        Node currentNode=head;
+        while(currentNode.next!=null){
+            currentNode=currentNode.next;
+        }
+        currentNode.next=newNode;
     }
 
-    void printList(){
-        if(head==null){
+    void printList() {
+        if (head == null) {
             System.out.println("The list is empty");
         }
-        Node currentNode=head;
-        while(currentNode!=null){
-            System.out.print(currentNode.num+"->");
-            currentNode=currentNode.next;
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.num + "->");
+            currentNode = currentNode.next;
         }
         System.out.println("NULL");
     }
-    public static void main(String[] args){
-        NodeSum s=new NodeSum();
+
+    int listToNum(){
+        Node currentNode=head;
+        int num= 0;
+        int i=0;
+        while (i<size && currentNode!=null){
+            num=(int)(Math.pow(10,i)* currentNode.num)+num;
+            currentNode=currentNode.next;
+            i++;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        NodeSum s = new NodeSum();
         s.addNode(3);
         s.addNode(4);
         s.addNode(2);
         s.printList();
-        NodeSum s2=new NodeSum();
+        NodeSum s2 = new NodeSum();
         s2.addNode(4);
         s2.addNode(6);
         s2.addNode(5);
         s2.printList();
+        int num1=s.listToNum();
+        int num2=s2.listToNum();
+        int res=num1+num2;
+        NodeSum result=new NodeSum();
+        while(res!=0){
+            result.addLast(res%10);
+            res=res/10;
+        }
+        result.printList();
     }
 }
