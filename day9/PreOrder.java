ce1b7fe392d0c1fdf660719d8d30376ecb1f5564
Murari Nagendra <70743564+devil056@users.noreply.github.com>
2022-08-05 15:40:26 +0530

    Organised

--- day9/PreOrder.java
+++ day9/PreOrder.java
@@ -1,31 +1,22 @@
-class Node4 {
+class Node4{
     int item;
-
     Node4 left, right;
-
-    public Node4(int key) {
-        item = key;
-        left = right = null;
+    public Node4(int key){
+        item=key;
+        left=right=null;
     }
 }
-
-class TraverseTree {
+class TraverseTree{
     Node4 root;
-
-    TraverseTree() {
-        root = null;
+    TraverseTree(){
+        root=null;
     }
-
-    void preOrder(Node4 node) {
-        if (node == null)
-            return;
-
-        System.out.print(node.item + "->");
-
+    void preOrder(Node4 node){
+        if(node==null) return;
+        System.out.print(node.item+"->");
         preOrder(node.left);
         preOrder(node.right);
     }
-
     void inOrder(Node4 node) {
         if (node == null) return;
         inOrder(node.left);
@@ -34,22 +25,26 @@ class TraverseTree {
     }
     void postOrder(Node4 node) {
         if (node == null) return;
-        inOrder(node.left);
-        inOrder(node.right);
+        postOrder(node.left);
+        postOrder(node.right);
         System.out.print(node.item + "->");
     }
 
-    public static void main(String args[]) {
-        TraverseTree tre1 = new TraverseTree();
-        tre1.root = new Node4(1);
-        tre1.root.left = new Node4(12);
-        tre1.root.right = new Node4(9);
-        tre1.root.left.left = new Node4(11);
-        tre1.root.left.right = new Node4(6);
-        System.out.println("Preorder Traversal->");
-        tre1.preOrder(tre1.root);
-        tre1.inOrder(tre1.root);
-        tre1.postOrder(tre1.root);
+    public static void main(String[] args) {
+        TraverseTree tree1=new TraverseTree();
+        tree1.root=new Node4(1);
+        tree1.root.left=new Node4(12);
+        tree1.root.right=new Node4(9);
+        tree1.root.left.left=new Node4(11);
+        tree1.root.left.right=new Node4(6);
+        System.out.print("Preorder traversal->");
+        tree1.preOrder(tree1.root);
+        System.out.println();
+        System.out.print("Inorder traversal: ");
+        tree1.inOrder(tree1.root);
+        System.out.println();
+        System.out.print("Post order traversal: ");
+        tree1.postOrder(tree1.root);
+
     }
 }
-
