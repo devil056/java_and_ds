e24065f796f649c2cfb8a9ee87473044d91b27cd
Murari Nagendra <70743564+devil056@users.noreply.github.com>
2022-08-05 09:53:57 +0530

    Regex first code

---
+++ day9/RegexLearning.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexLearning {
+
+    public static void main(String[] args){
+        List<String> emails=new ArrayList<String>();
+
+        emails.add("murari0418@gmail.com");
+        String regex="^(.+)@(.+)$";
+        Pattern pattern1=Pattern.compile(regex);
+        for(String email:emails){
+            Matcher matcher=pattern1.matcher(email);
+            System.out.println(email+" : "+matcher.matches());
+        }
+    }
+}
--- day9/first.java
+++
@@ -1,5 +0,0 @@
-public class first {
-    public static void main(String[] args){
-        System.out.println("Code 1 for the day and check push to github");
-    }
-}
